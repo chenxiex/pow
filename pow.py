87782b7d6b1501d4b1ee45482a3cdb5bf4fc0601
Chen Xi <chenxiex@outlook.com>
2024-03-20 16:22:25 +0800

    Add mypow()

---
+++ pow.py
@@ -0,0 +1,16 @@
+def mypow(a,b,m=0):
+    result=1
+    while b>0 :
+        if (b&1==1):
+            result=result*a
+            if m!=0:
+                result%=m
+        a*=a
+        if m!=0:
+            a%=m
+        b>>=1
+    return result
+
+if __name__=='__main__':
+    read(a,b,m)
+    print(mypow(a,b,m))
